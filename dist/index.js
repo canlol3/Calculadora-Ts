ca917d0989d4ba7a1219f10104382094b7594aed
Jose María López González <jlopgon0807b@cpifpalanturing.es>
2025-09-25 10:17:37 +0200

    final

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,54 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
     const display = document.getElementById("operacion");
-    const buttonsContainer = document.querySelector('.buttons');
-    buttonsContainer.addEventListener('click', (event) => {
+    const buttonsContainer = document.querySelector(".buttons");
+    buttonsContainer.addEventListener("click", (event) => {
+        var _a, _b, _c;
         const target = event.target;
-        if (target.classList.contains('btn')) {
-            if (target.classList.contains('number')) {
-                if (display.textContent.length >= 18) {
-                    document.body.style.pointerEvents = 'none';
-                    var temp = display.textContent;
-                    display.textContent = "MAX DIGITOS";
-                    setTimeout(() => {
-                        display.textContent = temp;
-                        document.body.style.pointerEvents = 'auto';
-                    }, 2000);
+        if (target.classList.contains("btn")) {
+            var digitos = (_a = display.textContent) === null || _a === void 0 ? void 0 : _a.length;
+            if (target.classList.contains("number")) {
+                var num = target.getAttribute("data-value");
+                if (digitos == 1 && display.textContent == "0") {
+                    display.textContent = num;
                 }
-                else {
-                    display.textContent += target.getAttribute('data-value');
+                else if (display && display.textContent && digitos >= 18) {
+                    mensajeAviso("MAX DIGITOS", 2000);
+                }
+                else if (num != "0") {
+                    display.textContent += num;
+                }
+                else if (display && display.textContent && digitos > 0) {
+                    display.textContent += num;
                 }
             }
-            else if (target.classList.contains('operator')) {
-                if (target.textContent == "=") {
-                    display.textContent = eval(display.textContent + "");
+            else if (target.classList.contains("operator")) {
+                var operator = target.getAttribute("data-value");
+                if (digitos == 1 && display.textContent == "0") {
+                    display.textContent = operator;
+                }
+                else if (target.textContent == "=") {
+                    display.textContent = ((_b = display.textContent) === null || _b === void 0 ? void 0 : _b.replace("√", "Math.sqrt")) + "";
+                    display.textContent = ((_c = display.textContent) === null || _c === void 0 ? void 0 : _c.replace("^", "**")) + "";
+                    console.log(display.textContent);
+                    if (eval(display.textContent + "") == Infinity) {
+                        mensajeAviso("MATH ERROR", 2000);
+                    }
+                    else {
+                        display.textContent = eval(display.textContent + "");
+                    }
                 }
                 else if (target.textContent == "⌫") {
                     display.textContent = display.textContent.slice(0, -1);
+                    if (digitos <= 1) {
+                        display.textContent = "0";
+                    }
                 }
                 else if (target.textContent == "C") {
-                    display.textContent = "";
+                    display.textContent = "0";
+                }
+                else if (target.textContent == "√") {
+                    display.textContent += "√(";
                 }
                 else {
                     display.textContent += target.textContent;
@@ -35,4 +56,16 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     });
+    function mensajeAviso(mensaje, tiempo) {
+        document.body.style.pointerEvents = "none";
+        var temp = display.textContent;
+        display.textContent = mensaje;
+        setTimeout(() => {
+            display.textContent = temp;
+            document.body.style.pointerEvents = "auto";
+        }, tiempo);
+    }
+    function filtrarCalculo(calculo) {
+        calculo.replace("√", "Math.sqrt");
+    }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const display = document.getElementById("operacion") as HTMLParagraphElement
+  const display = document.getElementById("operacion") as HTMLParagraphElement;
 
-  const buttonsContainer = document.querySelector('.buttons') as HTMLElement;
+  const buttonsContainer = document.querySelector(".buttons") as HTMLElement;
 
-  buttonsContainer.addEventListener('click', (event) => {
-    const target = event.target as HTMLElement
-    if(target.classList.contains('btn')){
-      if(target.classList.contains('number')){
-        if(display.textContent.length>=18){
-          document.body.style.pointerEvents = 'none';
-          var temp = display.textContent
-          display.textContent = "MAX DIGITOS"
-          setTimeout(()=>{
-            display.textContent = temp
-            document.body.style.pointerEvents = 'auto';
-          },2000)
-        }else{
-        display.textContent += target.getAttribute('data-value')
+  buttonsContainer.addEventListener("click", (event) => {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains("btn")) {
+      var digitos = display.textContent?.length!;
+      if (target.classList.contains("number")) {
+        var num = target.getAttribute("data-value");
+        if (digitos == 1 && display.textContent == "0") {
+          display.textContent = num;
+        } else if (display && display.textContent && digitos >= 18) {
+          mensajeAviso("MAX DIGITOS", 2000);
+        } else if (num != "0") {
+          display.textContent += num!;
+        } else if (display && display.textContent && digitos > 0) {
+          display.textContent += num;
         }
-      }else if(target.classList.contains('operator') ){
-        if(target.textContent == "="){
-         display.textContent = eval(display.textContent + "")
-        }else if(target.textContent =="⌫"){
-          display.textContent = display.textContent.slice(0,-1)
-        }else if(target.textContent == "C"){
-          display.textContent = ""
-        }else{
-          display.textContent += target.textContent
+      } else if (target.classList.contains("operator")) {
+        var operator = target.getAttribute("data-value");
+        if (digitos == 1 && display.textContent == "0") {
+          display.textContent = operator;
+        } else if (target.textContent == "=") {
+          display.textContent = display.textContent?.replace("√", "Math.sqrt") + "";  
+          display.textContent = display.textContent?.replace("^", "**") + "";  
+
+          console.log(display.textContent);
+          if (eval(display.textContent + "") == Infinity) {
+            mensajeAviso("MATH ERROR", 2000);
+          } else {
+            display.textContent = eval(display.textContent + "");
+          }
+        } else if (target.textContent == "⌫") {
+          display.textContent = display.textContent!.slice(0, -1);
+          if (digitos <= 1) {
+            display.textContent = "0";
+          }
+        } else if (target.textContent == "C") {
+          display.textContent = "0";
+        } else if (target.textContent == "√") {
+          display.textContent += "√(";
+        } else {
+          display.textContent += target.textContent!;
         }
       }
-    } 
-  })
-})
+    }
+  });
+  function mensajeAviso(mensaje: string, tiempo: number) {
+    document.body.style.pointerEvents = "none";
+    var temp = display.textContent;
+    display.textContent = mensaje;
+    setTimeout(() => {
+      display.textContent = temp;
+      document.body.style.pointerEvents = "auto";
+    }, tiempo);
+  }
+  function filtrarCalculo(calculo: string) {
+    calculo.replace("√", "Math.sqrt");
+  }
+});
