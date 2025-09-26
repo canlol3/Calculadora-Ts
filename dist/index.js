725f36d2ded8cddabcde6de853b9628a8f017384
Jose María López González <jlopgon0807b@cpifpalanturing.es>
2025-09-26 14:27:55 +0200

    optimizacion

--- dist/index.js
+++ dist/index.js
@@ -3,59 +3,72 @@ document.addEventListener("DOMContentLoaded", () => {
     const display = document.getElementById("operacion");
     const buttonsContainer = document.querySelector(".buttons");
     buttonsContainer.addEventListener("click", (event) => {
-        var _a, _b, _c;
         const target = event.target;
         if (target.classList.contains("btn")) {
-            var digitos = (_a = display.textContent) === null || _a === void 0 ? void 0 : _a.length;
-            if (target.classList.contains("number")) {
-                var num = target.getAttribute("data-value");
-                if (digitos == 1 && display.textContent == "0") {
-                    display.textContent = num;
-                }
-                else if (display && display.textContent && digitos >= 18) {
-                    mensajeAviso("MAX DIGITOS", 2000);
-                }
-                else if (num != "0") {
-                    display.textContent += num;
-                }
-                else if (display && display.textContent && digitos > 0) {
-                    display.textContent += num;
-                }
-            }
-            else if (target.classList.contains("operator")) {
-                var operator = target.getAttribute("data-value");
-                if (digitos == 1 && display.textContent == "0") {
-                    display.textContent = operator;
-                }
-                else if (target.textContent == "=") {
-                    display.textContent = ((_b = display.textContent) === null || _b === void 0 ? void 0 : _b.replace("√", "Math.sqrt")) + "";
-                    display.textContent = ((_c = display.textContent) === null || _c === void 0 ? void 0 : _c.replace("^", "**")) + "";
-                    console.log(display.textContent);
-                    if (eval(display.textContent + "") == Infinity) {
-                        mensajeAviso("MATH ERROR", 2000);
-                    }
-                    else {
-                        display.textContent = eval(display.textContent + "");
-                    }
-                }
-                else if (target.textContent == "⌫") {
-                    display.textContent = display.textContent.slice(0, -1);
-                    if (digitos <= 1) {
-                        display.textContent = "0";
-                    }
-                }
-                else if (target.textContent == "C") {
-                    display.textContent = "0";
-                }
-                else if (target.textContent == "√") {
-                    display.textContent += "√(";
-                }
-                else {
-                    display.textContent += target.textContent;
-                }
-            }
+            funcionBtn(target);
         }
     });
+    function funcionBtn(target) {
+        var _a;
+        var digitos = (_a = display.textContent) === null || _a === void 0 ? void 0 : _a.length;
+        if (target.classList.contains("number")) {
+            funcionNum(target, digitos);
+        }
+        else if (target.classList.contains("operator")) {
+            funcionOperator(target, digitos);
+        }
+    }
+    function funcionNum(target, digitos) {
+        var num = target.getAttribute("data-value");
+        if (digitos == 1 && display.textContent == "0") {
+            display.textContent = num;
+        }
+        else if (display && display.textContent && digitos >= 18) {
+            mensajeAviso("MAX DIGITOS", 2000);
+        }
+        else if (num != "0") {
+            display.textContent += num;
+        }
+        else if (display && display.textContent && digitos > 0) {
+            display.textContent += num;
+        }
+    }
+    function funcionOperator(target, digitos) {
+        var operator = target.getAttribute("data-value");
+        if (digitos == 1 && display.textContent == "0") {
+            display.textContent = operator;
+        }
+        if (target.textContent == "=") {
+            calculate();
+        }
+        else if (target.textContent == "⌫") {
+            display.textContent = display.textContent.slice(0, -1);
+            if (digitos <= 1) {
+                display.textContent = "0";
+            }
+        }
+        else if (target.textContent == "C") {
+            display.textContent = "0";
+        }
+        else if (target.textContent == "√") {
+            display.textContent += "√(";
+        }
+        else {
+            display.textContent += target.textContent;
+        }
+    }
+    function calculate() {
+        var _a, _b;
+        display.textContent = ((_a = display.textContent) === null || _a === void 0 ? void 0 : _a.replace("√", "Math.sqrt")) + "";
+        display.textContent = ((_b = display.textContent) === null || _b === void 0 ? void 0 : _b.replace("^", "**")) + "";
+        console.log(display.textContent);
+        if (eval(display.textContent + "") == Infinity) {
+            mensajeAviso("MATH ERROR", 2000);
+        }
+        else {
+            display.textContent = eval(display.textContent + "");
+        }
+    }
     function mensajeAviso(mensaje, tiempo) {
         document.body.style.pointerEvents = "none";
         var temp = display.textContent;
@@ -64,8 +77,5 @@ document.addEventListener("DOMContentLoaded", () => {
             display.textContent = temp;
             document.body.style.pointerEvents = "auto";
         }, tiempo);
-    }
-    function filtrarCalculo(calculo) {
-        calculo.replace("√", "Math.sqrt");
     }
 });
--- src/index.ts
+++ src/index.ts
@@ -6,47 +6,60 @@ document.addEventListener("DOMContentLoaded", () => {
   buttonsContainer.addEventListener("click", (event) => {
     const target = event.target as HTMLElement;
     if (target.classList.contains("btn")) {
-      var digitos = display.textContent?.length!;
-      if (target.classList.contains("number")) {
-        var num = target.getAttribute("data-value");
-        if (digitos == 1 && display.textContent == "0") {
-          display.textContent = num;
-        } else if (display && display.textContent && digitos >= 18) {
-          mensajeAviso("MAX DIGITOS", 2000);
-        } else if (num != "0") {
-          display.textContent += num!;
-        } else if (display && display.textContent && digitos > 0) {
-          display.textContent += num;
-        }
-      } else if (target.classList.contains("operator")) {
-        var operator = target.getAttribute("data-value");
-        if (digitos == 1 && display.textContent == "0") {
-          display.textContent = operator;
-        } else if (target.textContent == "=") {
-          display.textContent = display.textContent?.replace("√", "Math.sqrt") + "";  
-          display.textContent = display.textContent?.replace("^", "**") + "";  
-
-          console.log(display.textContent);
-          if (eval(display.textContent + "") == Infinity) {
-            mensajeAviso("MATH ERROR", 2000);
-          } else {
-            display.textContent = eval(display.textContent + "");
-          }
-        } else if (target.textContent == "⌫") {
-          display.textContent = display.textContent!.slice(0, -1);
-          if (digitos <= 1) {
-            display.textContent = "0";
-          }
-        } else if (target.textContent == "C") {
-          display.textContent = "0";
-        } else if (target.textContent == "√") {
-          display.textContent += "√(";
-        } else {
-          display.textContent += target.textContent!;
-        }
-      }
+      funcionBtn(target);
     }
   });
+  function funcionBtn(target: HTMLElement) {
+    var digitos = display.textContent?.length!;
+    if (target.classList.contains("number")) {
+      funcionNum(target, digitos);
+    } else if (target.classList.contains("operator")) {
+      funcionOperator(target, digitos);
+    }
+  }
+  function funcionNum(target: HTMLElement, digitos: number) {
+    var num = target.getAttribute("data-value");
+    if (digitos == 1 && display.textContent == "0") {
+      display.textContent = num;
+    } else if (display && display.textContent && digitos >= 18) {
+      mensajeAviso("MAX DIGITOS", 2000);
+    } else if (num != "0") {
+      display.textContent += num!;
+    } else if (display && display.textContent && digitos > 0) {
+      display.textContent += num;
+    }
+  }
+  function funcionOperator(target: HTMLElement, digitos: number) {
+    var operator = target.getAttribute("data-value");
+    if (digitos == 1 && display.textContent == "0") {
+      display.textContent = operator;
+    }
+    if (target.textContent == "=") {
+        calculate()
+    } else if (target.textContent == "⌫") {
+      display.textContent = display.textContent!.slice(0, -1);
+      if (digitos <= 1) {
+        display.textContent = "0";
+      }
+    } else if (target.textContent == "C") {
+      display.textContent = "0";
+    } else if (target.textContent == "√") {
+      display.textContent += "√(";
+    } else {
+      display.textContent += target.textContent!;
+    }
+  }
+  function calculate() {
+    display.textContent = display.textContent?.replace("√", "Math.sqrt") + "";
+    display.textContent = display.textContent?.replace("^", "**") + "";
+
+    console.log(display.textContent);
+    if (eval(display.textContent + "") == Infinity) {
+      mensajeAviso("MATH ERROR", 2000);
+    } else {
+      display.textContent = eval(display.textContent + "");
+    }
+  }
   function mensajeAviso(mensaje: string, tiempo: number) {
     document.body.style.pointerEvents = "none";
     var temp = display.textContent;
@@ -55,8 +68,5 @@ document.addEventListener("DOMContentLoaded", () => {
       display.textContent = temp;
       document.body.style.pointerEvents = "auto";
     }, tiempo);
-  }
-  function filtrarCalculo(calculo: string) {
-    calculo.replace("√", "Math.sqrt");
   }
 });
